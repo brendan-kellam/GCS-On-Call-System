e8eac870de123987e9254db5f63fcbb1e29c1da3
Brendan <bshizzle1234@gmail.com>
2015-06-04 17:33:23 -0400

    Completed admin page.

--- OnCallSystem/oncall/admin.py
+++ OnCallSystem/oncall/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Choice, Question
 
 # Register your models here.
+
+#Create a new stacked inline set of choices
+class ChoiceInline(admin.TabularInline):
+    
+    #set the model
+    model = Choice
+    
+    #set the original count (NOTE: These cannot be removed)
+    extra = 3
+
+#A model admin object for the Question model
+class QuestionAdmin(admin.ModelAdmin):
+    
+    #create a new field
+    fieldsets = [
+                 
+        #index 0 of each tuple is the title of the field
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}), #uses collapse to hide/show information
+    ]
+    
+    inlines = [ChoiceInline];
+    
+    #adds a new list display
+    list_display = ('question_text', 'pub_date', 'was_recently_published')
+    
+    #adds a new list display filter
+    list_filter = ['pub_date']
+    
+    #add a new search bar
+    search_fields = ['question_text']
+    
+admin.site.register(Question, QuestionAdmin);
--- OnCallSystem/oncall/models.py
+++ OnCallSystem/oncall/models.py
@@ -1,3 +1,31 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Question(models.Model): #inherits from models.Model
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    
+    #set a string representation for the model
+    def __str__(self):
+        return self.question_text
+    
+    def was_recently_published(self):
+        return self.pub_date > timezone.now() - datetime.timedelta(days=1)
+    
+    #sets admin properties
+    was_recently_published.admin_order_field = 'pub_date'
+    was_recently_published.boolean = True
+    was_recently_published.short_description = 'Published recently?'
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
+    #set a string representation for the model
+    def __str__(self):
+        return self.choice_text
